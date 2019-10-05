c7d61b4dc769370bfe78d5d05bca8f0fde003ad2
Dinwy <whix2010@gmail.com>
2019-10-05 11:18:32 +0900

    Setup basic things for intro, title, game scenes

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace LDJam45
+{
+	public class IntroManager : MonoBehaviour
+	{
+		public TextMeshProUGUI IntroText;
+
+		void Start()
+		{
+			Sequence seq = DOTween.Sequence();
+			seq.PrependInterval(0.5f);
+			IntroText.color = new Color(1, 1, 1, 0);
+
+			DOTween.ToAlpha(() => IntroText.color, x => IntroText.color = x, 1, 2)
+				.OnComplete(() =>
+					DOTween.ToAlpha(() => IntroText.color, x => IntroText.color = x, 0, 2)
+							.OnComplete(() => SceneManager.LoadSceneAsync(1)));
+		}
+	}
+}
---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,37 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace LDJam45
+{
+	public class TitleManager : MonoBehaviour
+	{
+		public Button WorldMap;
+		public Button Exit;
+
+		void Start()
+		{
+			WorldMap.onClick.AddListener(OnClickWorldMap);
+			Exit.onClick.AddListener(OnClickExit);
+		}
+
+		void OnDestroy()
+		{
+			WorldMap.onClick.RemoveListener(OnClickWorldMap);
+			Exit.onClick.RemoveListener(OnClickExit);
+		}
+
+		public void OnClickWorldMap()
+		{
+			// Which is Game
+			SceneManager.LoadSceneAsync(2);
+		}
+
+		public void OnClickExit()
+		{
+			Application.Quit();
+		}
+	}
+}
