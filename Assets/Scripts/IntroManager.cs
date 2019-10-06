37a787919f8304358baa461c1326ffacd45bad76
Dinwy <whix2010@gmail.com>
2019-10-06 16:04:35 +0900

    Modify intro scene

--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -1,14 +1,17 @@
-﻿using DG.Tweening;
-using TMPro;
+﻿
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
+using DG.Tweening;
+using TMPro;
 
 namespace LDJam45
 {
 	public class IntroManager : MonoBehaviour
 	{
 		public TextMeshProUGUI IntroText;
+		public Text textMessage;
 
 		void Start()
 		{
@@ -16,10 +19,15 @@ namespace LDJam45
 			seq.PrependInterval(0.5f);
 			IntroText.color = new Color(1, 1, 1, 0);
 
-			DOTween.ToAlpha(() => IntroText.color, x => IntroText.color = x, 1, 2)
-				.OnComplete(() =>
-					DOTween.ToAlpha(() => IntroText.color, x => IntroText.color = x, 0, 2)
-							.OnComplete(() => SceneManager.LoadSceneAsync(1)));
+			seq.Append(DOTween.ToAlpha(() => IntroText.color, x => IntroText.color = x, 1, 2));
+			seq.Append(textMessage.DOText("LDJam45", 3f));
+			seq.Append(DOTween.ToAlpha(() => IntroText.color, x => IntroText.color = x, 0, 2));
+			seq.AppendCallback(() => SceneManager.LoadSceneAsync(1));
+		}
+
+		void Update()
+		{
+			IntroText.text = textMessage.text;
 		}
 	}
 }
